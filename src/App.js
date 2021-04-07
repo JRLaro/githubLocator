aeae1116f96da67ea3947f0e5e7353942adfc120
J.Laro <joselarojr@gmail.com>
2021-04-07 18:29:09 -0400

    react router setup

--- src/App.js
+++ src/App.js
@@ -1,55 +1,74 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
 import axios from "axios";
 import Search from "./components/users/Search";
-import Alert from "./components/layout/Alert"
+import Alert from "./components/layout/Alert";
+import About from "./components/pages/About"
 
-class App extends React.Component {
+class App extends Component {
   state = {
     users: [],
     loading: false,
-    alert: null
+    alert: null,
   };
 
   searchUsers = async (text) => {
     this.setState({ loading: true });
 
     const res = await axios.get(
-      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
 
     this.setState({ users: res.data.items, loading: false });
-  }
+  };
 
   //clear Users from State
   clearUsers = () => {
-    this.setState({users: [], loading: false})
-  }
+    this.setState({ users: [], loading: false });
+  };
 
   setAlert = (message, type) => {
     this.setState({ alert: { message, type } });
-    setTimeout(()=> this.setState({alert: null}), 5000)
-}
+    setTimeout(() => this.setState({ alert: null }), 5000);
+  };
 
   render() {
-const { users, loading, alert} = this.state
-
+    const { users, loading, alert } = this.state;
 
     return (
-      <div className="App">
-        <Navbar />
-        <div className="container">
-          <Alert alert={alert}/>
-          <Search
-            searchUsers={this.searchUsers}
-            clearUsers={this.clearUsers}
-            showClear={users.length > 0 ? true : false}
-            setAlert={this.setAlert}
-          />
-          <Users loading={loading} users={users} />
+      <Router>
+        <div className="App">
+          <Navbar />
+          <div className="container">
+            <Alert alert={alert} />
+            <Switch>
+              <Route
+                exact
+                path="/"
+                render={(props) => (
+                  <Fragment>
+                    <Search
+                      searchUsers={this.searchUsers}
+                      clearUsers={this.clearUsers}
+                      showClear={users.length > 0 ? true : false}
+                      setAlert={this.setAlert}
+                    />
+                    <Users loading={loading} users={users} />
+                  </Fragment>
+                )}
+              />
+              <Route
+              exact
+                path='/about'
+                component={About}
+           />
+            </Switch>
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,24 +1,29 @@
-import React from 'react'
-import * as siIcons from 'react-icons/si'
-import PropTypes from 'prop-types'
+import React from "react";
+import * as siIcons from "react-icons/si";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
-
-const Navbar = ({title}) =>  {
-    
-        return (
-            <nav className="navbar bg-dark">
-                <h1>
-                <i className="icon"> <siIcons.SiGithub/> </i>
-                    {title} </h1>
-            </nav>
-        )
-
-}
+const Navbar = ({ title }) => {
+  return (
+    <nav className="navbar bg-dark">
+      <h1>
+        <i className="icon">
+          <siIcons.SiGithub className="mb-2" />
+        </i>
+        {title}
+          </h1>
+          <ul>
+              <li><Link to='/'> Home </Link></li>
+              <li><Link to='/about'>About</Link></li>
+          </ul>
+    </nav>
+  );
+};
 Navbar.defaultProps = {
-    title: 'GitHub Finder',
-}
+  title: "GitHub Finder",
+};
 
 Navbar.propTypes = {
-    title: PropTypes.string.isRequired,
-}
-export default Navbar
+  title: PropTypes.string.isRequired,
+};
+export default Navbar;
